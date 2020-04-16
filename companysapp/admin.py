d30c817ac154c0c7336640ee4b90939ce6c64eae
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2020-04-16 15:27:44 +0800

    Update admin.py

--- companysapp/admin.py
+++ companysapp/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from companysapp.models import company6666 as company
 
+#class companyAdmin(admin.ModelAdmin):
+#    # 第三種方式，加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
+#        list_display=('id','cName','cSex','cBirthday','cEmail','cPhone','cAddr')
+#        list_filter=('cName','cSex')
+#        search_fields=('cName',)
+#        ordering=('id',)
+
 class companyAdmin(admin.ModelAdmin):
     # 第三種方式，加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
-	list_display=('id','cName','cSex','cBirthday','cEmail','cPhone','cAddr')
-	list_filter=('cName','cSex')
-	search_fields=('cName',)
-	ordering=('id',)
-	
+	list_display=('idno','index','title','name','ename','stocks')
+	list_filter=(,'title','name','stocks')
+	search_fields=('name',)
+	ordering=('idno',)
 admin.site.register(company,companyAdmin)
 
 	
