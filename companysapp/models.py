0340b34261e08edadf166b4618d569da425bf84f
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2020-04-16 15:28:58 +0800

    Update models.py

--- companysapp/models.py
+++ companysapp/models.py
@@ -10,3 +10,15 @@ class company(models.Model):
 	
 	def __str__(self):
 		return self.cName
+
+	
+class company6666(models.Model):
+	idno = models.IntegerField(null=False)
+	index = models.IntegerField(null=False)
+	title = models.TextField(null=False)
+	name = models.TextField(null=False)
+	ename = models.TextField(null=True)
+	stocks = models.IntegerField(null=False)
+	
+	def __str__(self):
+		return self.idno
